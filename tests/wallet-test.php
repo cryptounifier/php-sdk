3feb12328e817eb44ef4ad2627334a82a5c47a59
miguilimzero <35383529+miguilimzero@users.noreply.github.com>
2024-12-20 01:59:18 -0300

    Update tests

--- tests/wallet-test.php
+++ tests/wallet-test.php
@@ -3,16 +3,22 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use CryptoUnifier\Api\WalletAPI;
-use GuzzleHttp\Exception\BadResponseException;
+// use GuzzleHttp\Exception\BadResponseException;
 
-$client = new WalletAPI('', '', 'trx');
+$client = new WalletAPI(
+    baseUrl: '', 
+    identifierKey: '', 
+    secretKey: '==',
+    symbol: 'trx',
+    network: 'mainnet'
+);
 
-var_dump($client->getDepositAddresses());
+var_dump($client->generateAddresses());
 
 var_dump($client->getBalance());
 
-try {
-    echo $client->sendTransaction(['DSMe7j5vVRgXRynJAACmjp8JQoFvJ7ud99' => 1])->message->txid;
-} catch (BadResponseException $e) {
-    var_dump(json_decode($e->getResponse()->getBody()));
-}
+// try {
+//     echo $client->sendTransaction(['DSMe7j5vVRgXRynJAACmjp8JQoFvJ7ud99' => 1])->message->txid;
+// } catch (BadResponseException $e) {
+//     var_dump(json_decode($e->getResponse()->getBody()));
+// }
